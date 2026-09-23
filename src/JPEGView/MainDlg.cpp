02b4a846501031e9ca405ddf09f360e265d9c93e
azizr12 <marissaxif@gmail.com>
2026-09-23 06:34:46 +0000

    Update MainDlg.cpp

--- src/JPEGView/MainDlg.cpp
+++ src/JPEGView/MainDlg.cpp
@@ -552,8 +552,8 @@ LRESULT CMainDlg::OnPaint(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, B
 		}
 	}
 
-/*GF*/	if (m_DynDwmFlush)
-/*GF*/		m_DynDwmFlush();	// Wait for vsync
+	if (m_DynDwmFlush)
+		m_DynDwmFlush();	// Wait for vsync
 
 	CPaintDC dc(m_hWnd);
 	m_dRealizedZoom = 1.0;
