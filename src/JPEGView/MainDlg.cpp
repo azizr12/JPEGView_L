e8baf566b0fe564b189f54a401232ae9827f490b
azizr12 <marissaxif@gmail.com>
2026-09-23 06:12:08 +0000

    Update MainDlg.cpp

--- src/JPEGView/MainDlg.cpp
+++ src/JPEGView/MainDlg.cpp
@@ -4661,7 +4661,7 @@ void CMainDlg::AnimateTransition() {
 					paintDC.BitBlt(0, 0, nW, nH, memDC, 0, 0, SRCCOPY);
 				} else {
 					float fFactor = (float)i / nSteps ;
-					blendFunc.SourceConstantAlpha = min(255, (int)((fFactor * fFactor * i + 1) * fAlphaStep + 0.5f));
+					blendFunc.SourceConstantAlpha = (BYTE)(fFactor * 255.0f + 0.5f);
 					paintDC.AlphaBlend(0, 0, nW, nH, memDC, 0, 0, nW, nH, blendFunc);
 				}
 				break;
